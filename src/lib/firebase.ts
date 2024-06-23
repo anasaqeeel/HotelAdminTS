9aaaca04dd68bfea90d1a287ca21ac29c185a2ec
Anas Aqeel <aqeelanas352@gmail.com>
2024-06-24 01:02:11 +0500

    add Firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLu5AM41y7DWR_oBGSY3aGUE_mG98QrRQ",
+  authDomain: "hoteladmints.firebaseapp.com",
+  projectId: "hoteladmints",
+  storageBucket: "hoteladmints.appspot.com",
+  messagingSenderId: "671355376603",
+  appId: "1:671355376603:web:39e542658c872eee0f4dfe",
+  measurementId: "G-KH7T6YEWCB"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
